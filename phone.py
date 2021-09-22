86eec5259dddf07d6bad3513cfd420bf5d4e480f
Evgeniy <vigi73@mail.ru>
2021-09-22 16:11:43 +0700

    Add files via upload

--- phone.py
+++ phone.py
@@ -1,6 +1,8 @@
-from peewee import *
+import re
+from models import List
 from PyQt5 import QtWidgets
-from mainForm import Ui_Form  # импорт нашего сгенерированного файла
+from PyQt5.QtWidgets import QTableWidgetItem
+from mainForm import Ui_Form
 import sys
 
 
@@ -11,9 +13,35 @@ class Main_window(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.setFixedSize(891, 444)
 
+    def get_all_value(self):
+        self.ui.table.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        # self.ui.table.setHorizontalHeaderItem(1, item)
+        #self.ui.table.horizontalHeader().setDefaultSectionSize(220)
+        self.ui.table.horizontalHeader().setSectionResizeMode(0, 1)
+        self.ui.table.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+
+
+        query = List.select()
+        for i, data in enumerate(query):
+            try:
+                mobil = re.findall(r'8-\d\d\d-\d\d\d-\d\d-\d\d', data.Fio)
+                fio = ' '.join(data.Fio.split()[:3])
+
+                self.ui.table.setRowCount(i + 1)
+                self.ui.table.setItem(i, 0, QTableWidgetItem(data.Name))
+
+                self.ui.table.setItem(i, 1, QTableWidgetItem(fio))
+                self.ui.table.setItem(i, 2, QTableWidgetItem(' '.join(mobil)))
+                self.ui.table.setItem(i, 3, QTableWidgetItem(data.Vts))
+                self.ui.table.setItem(i, 4, QTableWidgetItem(data.City))
+            except ValueError:
+                continue
+
 
 app = QtWidgets.QApplication([])
 application = Main_window()
+application.get_all_value()
 application.show()
 
 sys.exit(app.exec())
