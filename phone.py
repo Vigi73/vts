37f0925b74527c89948051be10f0d579e573b4ff
Evgeniy <vigi73@mail.ru>
2021-09-23 10:30:25 +0700

    Add files via upload

--- phone.py
+++ phone.py
@@ -2,45 +2,56 @@ import re
 from models import List
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QTableWidgetItem
+from PyQt5.QtCore import Qt
 from mainForm import Ui_Form
 import sys
 
 
 class Main_window(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self, query):
         super(Main_window, self).__init__()
         self.ui = Ui_Form()
         self.ui.setupUi(self)
-        self.setFixedSize(891, 444)
+        self.setFixedSize(891, 463)
+        self.query = query
+
+        self.ui.btnSearch.clicked.connect(self.search_name)
+        self.ui.inp.event()
 
     def get_all_value(self):
         self.ui.table.setRowCount(0)
-        item = QtWidgets.QTableWidgetItem()
-        # self.ui.table.setHorizontalHeaderItem(1, item)
-        #self.ui.table.horizontalHeader().setDefaultSectionSize(220)
         self.ui.table.horizontalHeader().setSectionResizeMode(0, 1)
-        self.ui.table.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+        # self.ui.table.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+        self.ui.table.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
 
-
-        query = List.select()
-        for i, data in enumerate(query):
+        # query = List.select()
+        for i, data in enumerate(self.query):
             try:
                 mobil = re.findall(r'8-\d\d\d-\d\d\d-\d\d-\d\d', data.Fio)
                 fio = ' '.join(data.Fio.split()[:3])
 
                 self.ui.table.setRowCount(i + 1)
-                self.ui.table.setItem(i, 0, QTableWidgetItem(data.Name))
+                self.ui.table.setItem(i, 0, QTableWidgetItem(data.Name.title()))
 
-                self.ui.table.setItem(i, 1, QTableWidgetItem(fio))
+                self.ui.table.setItem(i, 1, QTableWidgetItem(fio.title()))
                 self.ui.table.setItem(i, 2, QTableWidgetItem(' '.join(mobil)))
                 self.ui.table.setItem(i, 3, QTableWidgetItem(data.Vts))
                 self.ui.table.setItem(i, 4, QTableWidgetItem(data.City))
+
             except ValueError:
                 continue
+        self.ui.lcd.display(self.ui.table.rowCount())
+
+    def search_name(self):
+        self.query = List.select().where(
+            List.Fio ** f'%{self.ui.inp.text().upper()}%' |
+            List.Name ** f'%{self.ui.inp.text().upper()}%' |
+            List.Vts ** f'%{self.ui.inp.text().upper()}%')
+        self.get_all_value()
 
 
 app = QtWidgets.QApplication([])
-application = Main_window()
+application = Main_window(List.select())
 application.get_all_value()
 application.show()
 
